5e1fdec7ebdf626ae9aba8751a1bd3a1d40e0aa5
Qxver <paradzinskiwojciech@gmail.com>
2026-06-01 23:20:04 +0200

    card rarity

--- Scripts/Card/OpenCardPack.cs
+++ Scripts/Card/OpenCardPack.cs
@@ -14,11 +14,41 @@ public partial class OpenCardPack : Node{
 	for (int i = 0; i < CardsPerPack; i++)
 	{
 		int index = random.Next(allCards.Count);
-		CardData card = allCards[index];
+		CardData card = DrawWeightedCard(allCards, random);
 		CardCollection.Instance.AddCard(card);
 		pulled.Add(card);
 	}
 
 	return pulled;
 	}
+	
+	private CardData DrawWeightedCard(List<CardData> allCards, Random random)
+{
+	
+	
+	int DrawWeightedCard(int rarity) => rarity switch
+	{
+		1 => 40,
+		2 => 30,
+		3 => 20,
+		4 => 10,
+		_ => 1
+	};
+
+	int totalWeight = 0;
+	foreach (var card in allCards)
+		totalWeight += DrawWeightedCard(card.Rarity);
+
+	int roll = random.Next(totalWeight);
+	int total = 0;
+
+	foreach (var card in allCards)
+	{
+		total += DrawWeightedCard(card.Rarity);
+		if (roll < total)
+			return card;
+	}
+
+	return allCards[0];
+}
 }
