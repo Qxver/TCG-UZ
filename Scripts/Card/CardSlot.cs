a7598d94e6aa8c32adb7c893f81c9963e2691b7c
Adsekiii <adrianzurawski@proton.me>
2026-06-02 12:32:01 +0200

    [TCG-UZ]: Enemy

--- Scripts/Card/CardSlot.cs
+++ Scripts/Card/CardSlot.cs
@@ -5,4 +5,24 @@ public partial class CardSlot : Node2D
 {
 	[Export] public bool isPlayerSlot = true;
 	public bool cardInside = false;
+
+	public bool IsEmpty()
+	{
+		return !cardInside;
+	}
+
+	public void SpawnEnemy()
+	{
+		var cardScene = GD.Load<PackedScene>("res://Scenes/Card.tscn");
+		var cardData = GD.Load<CardData>("res://Cards/hladowski.tres"); // Default enemy
+		if (cardScene != null && cardData != null)
+		{
+			var card = cardScene.Instantiate<Card>();
+			GetTree().Root.GetNode("Game/CardManager").AddChild(card);
+			card.SetCardData(cardData);
+			card.GlobalPosition = this.GlobalPosition - card.PivotOffset;
+			cardInside = true;
+			card.isPlaced = true;
+		}
+	}
 }
